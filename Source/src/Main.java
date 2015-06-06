a66421744dc41bbf13fe5303637c82652d61d702
Wan Yuan <wanyuansg@163.com>
2015-06-06 17:31:38 +0800

    Initialize

---
+++ Source/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Yuan on 2015/6/6.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Source/src/test/Stage1.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by Yuan on 2015/6/6.
+ */
+public class Stage1 {
+}
